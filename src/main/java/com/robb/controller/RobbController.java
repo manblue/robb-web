9adfd41ed726685949c003fb1743fafcdd821e8a
maolong <maolong@laiyongche.com>
2018-11-13 18:29:27 +0800

    update

--- src/main/java/com/robb/controller/RobbController.java
+++ src/main/java/com/robb/controller/RobbController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class RobbController {
 	private RobbManager robbManager;
 	
 	
-	@RequestMapping("add")
+	@RequestMapping(value="add",method = RequestMethod.GET)
 	public RobbReponse add(String name) {
 		return robbManager.add(name);
 	}
